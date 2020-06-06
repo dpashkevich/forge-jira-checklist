31eb26501ef2c92c77ad27d2438c40d88b722c2f
dpashkevich <dpashk@gmail.com>
2020-06-05 22:24:41 -0400

    UI stub

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,58 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, {
+    render,
+    Fragment,
+    Text,
+    IssuePanel,
+    Form,
+    Button,
+    ButtonSet,
+    CheckboxGroup,
+    Checkbox,
+    useState,
+    Table,
+    Head,
+    Row,
+    Cell,
+    TextField
+} from '@forge/ui';
+
+const Task = ({text, isDone}) => (
+  <Row>
+    <Cell>
+      <Button text="☐" />
+    </Cell>
+    <Cell>
+      <Text>{text}</Text>
+    </Cell>
+    <Cell>
+      <Button text="✖" />
+    </Cell>
+  </Row>
+)
+
 
 const App = () => {
   return (
     <Fragment>
-      <Text>Hello world!</Text>
+      <Form>
+        <TextField name="newTask" placeholder="Enter new task" />
+      </Form>
+      <Table>
+        <Head>
+          <Cell><Text>**Tasks**</Text></Cell>
+          <Cell></Cell>
+          <Cell></Cell>
+        </Head>
+        {[
+          'Get milk',
+          'Write an app',
+          'Win CodeGeist 2020',
+          'Buy a private jet and retire on the Bahamas'
+         ].map((text) => (
+           <Task text={text} />
+         ))}
+
+      </Table>
     </Fragment>
   );
 };
